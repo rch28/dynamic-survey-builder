74df4ec583e59437ebf2964f92106db30a685340
Raju Chhetri <rajuchhetry11@gmail.com>
2025-05-14 08:28:35 +0545

    fix: Comment out output and distDir options in next.config.ts for clarity

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  distDir: "out",
+  // output: "export",
+  // distDir: "out",
   images: {
     remotePatterns: [
       {
